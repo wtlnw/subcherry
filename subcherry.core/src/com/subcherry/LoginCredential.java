862a15794400b32ecb18c4f388411a61da0f17ab
haumacher@googlemail.com <haumacher@googlemail.com@89d24fb1-277a-59bb-3637-86753941562b>
2013-03-16 22:14:40 +0000

    Allow storing base64 encoded passwords.

--- subcherry.core/src/com/subcherry/LoginCredential.java
+++ subcherry.core/src/com/subcherry/LoginCredential.java
@@ -2,7 +2,7 @@ package com.subcherry;
 
 import org.apache.commons.codec.binary.Base64;
 
-import de.haumacher.common.config.Parser;
+import de.haumacher.common.config.ObjectParser;
 import de.haumacher.common.config.Value;
 import de.haumacher.common.config.annotate.ValueParser;
 
@@ -11,13 +11,18 @@ import de.haumacher.common.config.annotate.ValueParser;
  */
 public interface LoginCredential extends Value {
 	
-	public static class PasswordDecoder implements Parser<String>{
+	public static class PasswordDecoder extends ObjectParser<String> {
 
 		@Override
 		public String parse(String input) {
 			return new String(Base64.decodeBase64(input));
 		}
 		
+		@Override
+		public String unparse(String value) {
+			return Base64.encodeBase64String(value.getBytes());
+		}
+
 	}
 	
 	String getUser();

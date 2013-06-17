5324866c547fcda2fa075b72a68402ac3bdb2488
haumacher@googlemail.com <haumacher@googlemail.com@89d24fb1-277a-59bb-3637-86753941562b>
2013-06-17 13:52:24 +0000

    Extracted commmit message rewriting to its own class.

---
+++ subcherry.core/src/com/subcherry/commit/MessageRewriter.java
@@ -0,0 +1,157 @@
+/*
+ * TimeCollect records time you spent on your development work.
+ * Copyright (C) 2013 Bernhard Haumacher and others
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.subcherry.commit;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.tmatesoft.svn.core.SVNLogEntry;
+
+import com.subcherry.Configuration;
+import com.subcherry.utils.Utils;
+import com.subcherry.utils.Utils.TicketMessage;
+
+public class MessageRewriter {
+
+	private final Configuration _config;
+
+	public MessageRewriter(Configuration config) {
+		_config = config;
+	}
+
+	public String resolvePortMessage(SVNLogEntry logEntry) {
+		String logEntryMessage = logEntry.getMessage();
+		if (CommitHandler.ORIGINAL.equals(_config.getPortMessage())) {
+			return logEntryMessage; 
+		}
+		if (CommitHandler.BACKPORT.equals(_config.getPortMessage())) {
+			return backPortMessage(logEntryMessage);
+		}
+		
+		TicketMessage message = new TicketMessage(logEntryMessage);
+	
+		StringBuilder newMesssage = new StringBuilder();
+		newMesssage.append("Ticket #");
+		newMesssage.append(message.ticketNumber);
+		newMesssage.append(": ");
+	
+		if (_config.getRebase()) {
+			if (message.isHotfix()) {
+				addHotfix(newMesssage);
+			} else if (message.isPreview()) {
+				addPreview(newMesssage);
+			} else if (message.isBranchChange()) {
+				addBranchChange(newMesssage);
+			} else if (message.isPort()) {
+				addPort(newMesssage);
+			}
+		}
+		else if (_config.getPreview()) {
+			addPreview(newMesssage);
+		} 
+		else {
+			if (!_config.getRevert()) {
+				addPort(newMesssage);
+			}
+		}
+	
+		if (message.apiChange != null) {
+			newMesssage.append("API change: ");
+		}
+		
+		if (_config.getRevert()) {
+			newMesssage.append("Reverted ");
+		}
+		newMesssage.append("[");
+		newMesssage.append(logEntry.getRevision());
+		newMesssage.append("]:");
+		
+		newMesssage.append(message.originalMessage);
+		return newMesssage.toString();
+	}
+
+	private String backPortMessage(String logEntryMessage) {
+		String branchName = getBranchName(_config.getSourceBranch());
+		Pattern pattern = Pattern.compile("Ticket #(\\d+): On " + branchName + ": (.*)", Pattern.DOTALL);
+		Matcher matcher = pattern.matcher(logEntryMessage);
+		if (!matcher.matches()) {
+			throw new IllegalStateException();
+		}
+		String ticketNumber = matcher.group(1);
+		String originalMessage = matcher.group(2);
+		
+		StringBuilder backportMessage = new StringBuilder("Ticket #");
+		backportMessage.append(ticketNumber);
+		backportMessage.append(": ");
+		
+		String targetBranch = getBranchName(_config.getTargetBranch());
+		if (!isTrunk(targetBranch)) {
+			backportMessage.append("On ");
+			backportMessage.append(targetBranch);
+			backportMessage.append(": ");
+		}
+		backportMessage.append(originalMessage);
+		return backportMessage.toString();
+	}
+
+	private void addHotfix(StringBuilder newMesssage) {
+		newMesssage.append("Hotfix for ");
+		newMesssage.append(getBranchName(_config.getTargetBranch()));
+		newMesssage.append(": ");
+	}
+
+	private void addPort(StringBuilder newMesssage) {
+		newMesssage.append("Ported to ");
+		newMesssage.append(getBranchName(_config.getTargetBranch()));
+		newMesssage.append(" from ");
+		newMesssage.append(getBranchName(_config.getSourceBranch()));
+		newMesssage.append(": ");
+	}
+
+	private void addPreview(StringBuilder newMesssage) {
+		newMesssage.append("Preview on ");
+		newMesssage.append(getBranchName(_config.getTargetBranch()));
+		newMesssage.append(": ");
+	}
+
+	private void addBranchChange(StringBuilder newMesssage) {
+		newMesssage.append("On ");
+		newMesssage.append(getBranchName(_config.getTargetBranch()));
+		newMesssage.append(": ");
+	}
+
+	private String getBranchName(String branch) {
+		Pattern trunkPattern = Pattern.compile("^/?(" + CommitHandler.TRUNK +")(?:/([^/]+))$");
+		Matcher trunkMatcher = trunkPattern.matcher(branch);
+		if (trunkMatcher.matches()) {
+			String category = trunkMatcher.group(2);
+			return (category != null ? category + "_" : "") + CommitHandler.TRUNK;
+		}
+		int index = branch.lastIndexOf(Utils.SVN_SERVER_PATH_SEPARATOR);
+		if (index == -1) {
+			return branch;
+		} else {
+			return branch.substring(index +1);
+		}
+	}
+
+	private boolean isTrunk(String branchName) {
+		return branchName.endsWith(CommitHandler.TRUNK);
+	}
+
+}

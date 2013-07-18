2652ead79c24a87feca51014ed45c7eab68f84cb
haumacher@googlemail.com <haumacher@googlemail.com@89d24fb1-277a-59bb-3637-86753941562b>
2013-07-18 08:45:24 +0000

    Follow-up for [82]: Bugfix: Print number of revisions instead of all toString() information.

--- subcherry.core/src/com/subcherry/Main.java
+++ subcherry.core/src/com/subcherry/Main.java
@@ -108,7 +108,7 @@ public class Main {
 		for (CommitSet commitSet : commitSets) {
 			commitSet.print(System.out);
 		}
-		Log.info("Start merging " + logEntryMatcher.getEntries() + " revisions.");
+		Log.info("Start merging " + logEntryMatcher.getEntries().size() + " revisions.");
 
 		mergeCommitHandler.run(commitSets);
 
